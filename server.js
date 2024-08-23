1daded0d4112d7b6c1773ea115ab21296a887507
MayurJirapure11 <mrjirapure@gmail.com>
2024-08-23 18:51:44 +0530

    Done Authentification

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const app = express();
 const db = require("./db");
 require('dotenv').config()
 
+const passport = require('./auth.js')
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
 
+const localAuthMiddleware = passport.authenticate('local',{session:false}) ;
 app.get("/", function (req, res) {
   res.send("Welcome Sir");
 });
 
 const personRoutes = require('./routes/personRoutes.js')
-app.use('/person',personRoutes)
+app.use('/person',localAuthMiddleware,personRoutes)
 
 const menuRoutes = require('./routes/menuRoutes.js')
 app.use('/menu',menuRoutes)
